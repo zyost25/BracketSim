54788856a926039841b5315e0875c618f89bb35b
zyost25 <zeke.yost@gmail.com>
2022-06-20 19:57:06 -0500

    Added more labels to left side of bracket

--- BracketSim/Bracket.Designer.cs
+++ BracketSim/Bracket.Designer.cs
@@ -29,7 +29,7 @@ namespace BracketSim
         /// </summary>
         private void InitializeComponent()
         {
-            this.leftLblOne = new System.Windows.Forms.Label();
+            this.leftLbl1 = new System.Windows.Forms.Label();
             this.leftLbl2 = new System.Windows.Forms.Label();
             this.leftLbl3 = new System.Windows.Forms.Label();
             this.leftLbl4 = new System.Windows.Forms.Label();
@@ -45,140 +45,147 @@ namespace BracketSim
             this.rightLbl3 = new System.Windows.Forms.Label();
             this.rightLbl2 = new System.Windows.Forms.Label();
             this.rightLbl1 = new System.Windows.Forms.Label();
+            this.leftLbl9 = new System.Windows.Forms.Label();
+            this.leftLbl10 = new System.Windows.Forms.Label();
+            this.leftLbl11 = new System.Windows.Forms.Label();
+            this.leftLbl12 = new System.Windows.Forms.Label();
+            this.leftLbl13 = new System.Windows.Forms.Label();
+            this.leftLbl14 = new System.Windows.Forms.Label();
+            this.leftLbl15 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // leftLblOne
+            // leftLbl1
             // 
-            this.leftLblOne.AutoSize = true;
-            this.leftLblOne.Location = new System.Drawing.Point(22, 24);
-            this.leftLblOne.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.leftLblOne.Name = "leftLblOne";
-            this.leftLblOne.Size = new System.Drawing.Size(15, 10);
-            this.leftLblOne.TabIndex = 0;
-            this.leftLblOne.Text = "1) ";
+            this.leftLbl1.AutoSize = true;
+            this.leftLbl1.Location = new System.Drawing.Point(28, 30);
+            this.leftLbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl1.Name = "leftLbl1";
+            this.leftLbl1.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl1.TabIndex = 0;
+            this.leftLbl1.Text = "1) ";
             // 
             // leftLbl2
             // 
             this.leftLbl2.AutoSize = true;
-            this.leftLbl2.Location = new System.Drawing.Point(22, 61);
+            this.leftLbl2.Location = new System.Drawing.Point(28, 76);
             this.leftLbl2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.leftLbl2.Name = "leftLbl2";
-            this.leftLbl2.Size = new System.Drawing.Size(15, 10);
+            this.leftLbl2.Size = new System.Drawing.Size(25, 17);
             this.leftLbl2.TabIndex = 1;
             this.leftLbl2.Text = "8) ";
             // 
             // leftLbl3
             // 
             this.leftLbl3.AutoSize = true;
-            this.leftLbl3.Location = new System.Drawing.Point(22, 102);
+            this.leftLbl3.Location = new System.Drawing.Point(28, 128);
             this.leftLbl3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.leftLbl3.Name = "leftLbl3";
-            this.leftLbl3.Size = new System.Drawing.Size(15, 10);
+            this.leftLbl3.Size = new System.Drawing.Size(25, 17);
             this.leftLbl3.TabIndex = 2;
             this.leftLbl3.Text = "4) ";
             // 
             // leftLbl4
             // 
             this.leftLbl4.AutoSize = true;
-            this.leftLbl4.Location = new System.Drawing.Point(22, 142);
+            this.leftLbl4.Location = new System.Drawing.Point(28, 178);
             this.leftLbl4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.leftLbl4.Name = "leftLbl4";
-            this.leftLbl4.Size = new System.Drawing.Size(15, 10);
+            this.leftLbl4.Size = new System.Drawing.Size(25, 17);
             this.leftLbl4.TabIndex = 3;
             this.leftLbl4.Text = "5) ";
             // 
             // leftLbl6
             // 
             this.leftLbl6.AutoSize = true;
-            this.leftLbl6.Location = new System.Drawing.Point(22, 223);
+            this.leftLbl6.Location = new System.Drawing.Point(28, 279);
             this.leftLbl6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.leftLbl6.Name = "leftLbl6";
-            this.leftLbl6.Size = new System.Drawing.Size(15, 10);
+            this.leftLbl6.Size = new System.Drawing.Size(25, 17);
             this.leftLbl6.TabIndex = 5;
             this.leftLbl6.Text = "6) ";
             // 
             // leftLbl5
             // 
             this.leftLbl5.AutoSize = true;
-            this.leftLbl5.Location = new System.Drawing.Point(22, 183);
+            this.leftLbl5.Location = new System.Drawing.Point(28, 229);
             this.leftLbl5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.leftLbl5.Name = "leftLbl5";
-            this.leftLbl5.Size = new System.Drawing.Size(15, 10);
+            this.leftLbl5.Size = new System.Drawing.Size(25, 17);
             this.leftLbl5.TabIndex = 4;
             this.leftLbl5.Text = "3) ";
             // 
             // leftLbl8
             // 
             this.leftLbl8.AutoSize = true;
-            this.leftLbl8.Location = new System.Drawing.Point(22, 305);
+            this.leftLbl8.Location = new System.Drawing.Point(28, 381);
             this.leftLbl8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.leftLbl8.Name = "leftLbl8";
-            this.leftLbl8.Size = new System.Drawing.Size(15, 10);
+            this.leftLbl8.Size = new System.Drawing.Size(25, 17);
             this.leftLbl8.TabIndex = 7;
             this.leftLbl8.Text = "7) ";
             // 
             // leftLbl7
             // 
             this.leftLbl7.AutoSize = true;
-            this.leftLbl7.Location = new System.Drawing.Point(22, 264);
+            this.leftLbl7.Location = new System.Drawing.Point(28, 330);
             this.leftLbl7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.leftLbl7.Name = "leftLbl7";
-            this.leftLbl7.Size = new System.Drawing.Size(15, 10);
+            this.leftLbl7.Size = new System.Drawing.Size(25, 17);
             this.leftLbl7.TabIndex = 6;
             this.leftLbl7.Text = "2) ";
             // 
             // rightLbl8
             // 
             this.rightLbl8.AutoSize = true;
-            this.rightLbl8.Location = new System.Drawing.Point(890, 305);
+            this.rightLbl8.Location = new System.Drawing.Point(1112, 381);
             this.rightLbl8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl8.Name = "rightLbl8";
             this.rightLbl8.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl8.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl8.Size = new System.Drawing.Size(25, 17);
             this.rightLbl8.TabIndex = 15;
             this.rightLbl8.Text = "7) ";
             // 
             // rightLbl7
             // 
             this.rightLbl7.AutoSize = true;
-            this.rightLbl7.Location = new System.Drawing.Point(890, 265);
+            this.rightLbl7.Location = new System.Drawing.Point(1112, 331);
             this.rightLbl7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl7.Name = "rightLbl7";
             this.rightLbl7.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl7.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl7.Size = new System.Drawing.Size(25, 17);
             this.rightLbl7.TabIndex = 14;
             this.rightLbl7.Text = "2) ";
             // 
             // rightLbl6
             // 
             this.rightLbl6.AutoSize = true;
-            this.rightLbl6.Location = new System.Drawing.Point(890, 225);
+            this.rightLbl6.Location = new System.Drawing.Point(1112, 281);
             this.rightLbl6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl6.Name = "rightLbl6";
             this.rightLbl6.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl6.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl6.Size = new System.Drawing.Size(25, 17);
             this.rightLbl6.TabIndex = 13;
             this.rightLbl6.Text = "6) ";
             // 
             // rightLbl5
             // 
             this.rightLbl5.AutoSize = true;
-            this.rightLbl5.Location = new System.Drawing.Point(890, 185);
+            this.rightLbl5.Location = new System.Drawing.Point(1112, 231);
             this.rightLbl5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl5.Name = "rightLbl5";
             this.rightLbl5.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl5.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl5.Size = new System.Drawing.Size(25, 17);
             this.rightLbl5.TabIndex = 12;
             this.rightLbl5.Text = "3) ";
             // 
             // rightLbl4
             // 
             this.rightLbl4.AutoSize = true;
-            this.rightLbl4.Location = new System.Drawing.Point(890, 145);
+            this.rightLbl4.Location = new System.Drawing.Point(1112, 181);
             this.rightLbl4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl4.Name = "rightLbl4";
             this.rightLbl4.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl4.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl4.Size = new System.Drawing.Size(25, 17);
             this.rightLbl4.TabIndex = 11;
             this.rightLbl4.Text = "5) ";
             // 
@@ -186,11 +193,11 @@ namespace BracketSim
             // 
             this.rightLbl3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.rightLbl3.AutoSize = true;
-            this.rightLbl3.Location = new System.Drawing.Point(890, 105);
+            this.rightLbl3.Location = new System.Drawing.Point(1112, 131);
             this.rightLbl3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl3.Name = "rightLbl3";
             this.rightLbl3.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl3.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl3.Size = new System.Drawing.Size(25, 17);
             this.rightLbl3.TabIndex = 10;
             this.rightLbl3.Text = "4) ";
             this.rightLbl3.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -198,30 +205,107 @@ namespace BracketSim
             // rightLbl2
             // 
             this.rightLbl2.AutoSize = true;
-            this.rightLbl2.Location = new System.Drawing.Point(890, 65);
+            this.rightLbl2.Location = new System.Drawing.Point(1112, 81);
             this.rightLbl2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl2.Name = "rightLbl2";
             this.rightLbl2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl2.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl2.Size = new System.Drawing.Size(25, 17);
             this.rightLbl2.TabIndex = 9;
             this.rightLbl2.Text = "8) ";
             // 
             // rightLbl1
             // 
             this.rightLbl1.AutoSize = true;
-            this.rightLbl1.Location = new System.Drawing.Point(890, 25);
+            this.rightLbl1.Location = new System.Drawing.Point(1112, 31);
             this.rightLbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rightLbl1.Name = "rightLbl1";
             this.rightLbl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rightLbl1.Size = new System.Drawing.Size(15, 10);
+            this.rightLbl1.Size = new System.Drawing.Size(25, 17);
             this.rightLbl1.TabIndex = 8;
             this.rightLbl1.Text = "1) ";
             // 
+            // leftLbl9
+            // 
+            this.leftLbl9.AutoSize = true;
+            this.leftLbl9.Location = new System.Drawing.Point(175, 55);
+            this.leftLbl9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl9.Name = "leftLbl9";
+            this.leftLbl9.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl9.TabIndex = 16;
+            this.leftLbl9.Text = "1) ";
+            // 
+            // leftLbl10
+            // 
+            this.leftLbl10.AutoSize = true;
+            this.leftLbl10.Location = new System.Drawing.Point(175, 155);
+            this.leftLbl10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl10.Name = "leftLbl10";
+            this.leftLbl10.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl10.TabIndex = 17;
+            this.leftLbl10.Text = "4) ";
+            // 
+            // leftLbl11
+            // 
+            this.leftLbl11.AutoSize = true;
+            this.leftLbl11.Location = new System.Drawing.Point(175, 255);
+            this.leftLbl11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl11.Name = "leftLbl11";
+            this.leftLbl11.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl11.TabIndex = 18;
+            this.leftLbl11.Text = "3) ";
+            // 
+            // leftLbl12
+            // 
+            this.leftLbl12.AutoSize = true;
+            this.leftLbl12.Location = new System.Drawing.Point(175, 355);
+            this.leftLbl12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl12.Name = "leftLbl12";
+            this.leftLbl12.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl12.TabIndex = 19;
+            this.leftLbl12.Text = "2) ";
+            // 
+            // leftLbl13
+            // 
+            this.leftLbl13.AutoSize = true;
+            this.leftLbl13.Location = new System.Drawing.Point(330, 105);
+            this.leftLbl13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl13.Name = "leftLbl13";
+            this.leftLbl13.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl13.TabIndex = 20;
+            this.leftLbl13.Text = "1) ";
+            // 
+            // leftLbl14
+            // 
+            this.leftLbl14.AutoSize = true;
+            this.leftLbl14.Location = new System.Drawing.Point(330, 305);
+            this.leftLbl14.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl14.Name = "leftLbl14";
+            this.leftLbl14.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl14.TabIndex = 21;
+            this.leftLbl14.Text = "2) ";
+            // 
+            // leftLbl15
+            // 
+            this.leftLbl15.AutoSize = true;
+            this.leftLbl15.Location = new System.Drawing.Point(480, 205);
+            this.leftLbl15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.leftLbl15.Name = "leftLbl15";
+            this.leftLbl15.Size = new System.Drawing.Size(25, 17);
+            this.leftLbl15.TabIndex = 22;
+            this.leftLbl15.Text = "1) ";
+            // 
             // Bracket
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1000, 350);
+            this.ClientSize = new System.Drawing.Size(1250, 438);
+            this.Controls.Add(this.leftLbl15);
+            this.Controls.Add(this.leftLbl14);
+            this.Controls.Add(this.leftLbl13);
+            this.Controls.Add(this.leftLbl12);
+            this.Controls.Add(this.leftLbl11);
+            this.Controls.Add(this.leftLbl10);
+            this.Controls.Add(this.leftLbl9);
             this.Controls.Add(this.rightLbl8);
             this.Controls.Add(this.rightLbl7);
             this.Controls.Add(this.rightLbl6);
@@ -237,7 +321,7 @@ namespace BracketSim
             this.Controls.Add(this.leftLbl4);
             this.Controls.Add(this.leftLbl3);
             this.Controls.Add(this.leftLbl2);
-            this.Controls.Add(this.leftLblOne);
+            this.Controls.Add(this.leftLbl1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Bracket";
             this.Text = "Bracket";
@@ -249,7 +333,7 @@ namespace BracketSim
 
         #endregion
 
-        private System.Windows.Forms.Label leftLblOne;
+        private System.Windows.Forms.Label leftLbl1;
         private System.Windows.Forms.Label leftLbl2;
         private System.Windows.Forms.Label leftLbl3;
         private System.Windows.Forms.Label leftLbl4;
@@ -265,6 +349,13 @@ namespace BracketSim
         private System.Windows.Forms.Label rightLbl3;
         private System.Windows.Forms.Label rightLbl2;
         private System.Windows.Forms.Label rightLbl1;
+        private System.Windows.Forms.Label leftLbl9;
+        private System.Windows.Forms.Label leftLbl10;
+        private System.Windows.Forms.Label leftLbl11;
+        private System.Windows.Forms.Label leftLbl12;
+        private System.Windows.Forms.Label leftLbl13;
+        private System.Windows.Forms.Label leftLbl14;
+        private System.Windows.Forms.Label leftLbl15;
     }
 }
 
