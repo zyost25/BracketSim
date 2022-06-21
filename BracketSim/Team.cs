7dd221eca31452f0bb936065fb8bf44bb0615332
Zeke <99764039+zyost25@users.noreply.github.com>
2022-06-21 09:32:32 -0500

    Started Team class

---
+++ BracketSim/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketSim
+{
+    class Team
+    {
+        public int seed { get; set; }
+        public string name { get; set; }
+    }
+}
